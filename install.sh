743f05dd951296ecd421a77f917da612fc9b5a7d
Dustin Savery <dusave@github.com>
2021-12-13 23:51:17 +0000

    adding gh cli to install

--- install.sh
+++ install.sh
@@ -23,6 +23,11 @@ if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-nvm ]; then
   git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 fi
 
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
 ln -s $(pwd)/zshrc $HOME/.zshrc
 ln -s $(pwd)/p10k.zsh $HOME/.p10k.zsh
 ln -s $(pwd)/gitignore_global $HOME/.gitignore_global
